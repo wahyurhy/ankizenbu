527d6ab18cf6071129bf428ea919b9e8b893d198
wahyurhy <wahyurahayu82@gmail.com>
2024-12-10 20:36:51 +0700

    update: search any of words for memorizing

--- app/src/main/java/com/wahyurhy/ankizenbu/MainActivity.kt
+++ app/src/main/java/com/wahyurhy/ankizenbu/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -27,10 +28,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -38,6 +43,7 @@ import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -51,6 +57,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -229,21 +237,111 @@ fun MyApp(context: Context, sendTextToExternalApp: (Context, String) -> Unit) {
                                 horizontalArrangement = Arrangement.SpaceBetween,
                                 verticalAlignment = Alignment.CenterVertically
                             ) {
-                                Text(
-                                    text = "Klik 1x: ${totalClicksByCount.value[1] ?: 0}",
-                                    color = Color(0xFFBB0000), // Tetap merah cerah
-                                    fontSize = 16.sp
+                                val showCategoryWords = remember { mutableStateOf<Pair<Int, List<String>>?>(null) }
+
+                                // Klik 1x
+                                ClickableText(
+                                    text = AnnotatedString("Klik 1x: ${totalClicksByCount.value[1] ?: 0}"),
+                                    style = TextStyle(
+                                        fontSize = 16.sp,
+                                        color = Color(0xFFBB0000) // Merah cerah
+                                    ),
+                                    onClick = {
+                                        val categoryWords = sharedPreferences.all
+                                            .filter { it.key.endsWith("_clickCount") && it.value == 1 }
+                                            .keys.map { it.removeSuffix("_clickCount") }
+                                        showCategoryWords.value = 1 to categoryWords
+                                    }
                                 )
-                                Text(
-                                    text = "Klik 2x: ${totalClicksByCount.value[2] ?: 0}",
-                                    color = Color(0xFF60D500), // Tetap hijau cerah
-                                    fontSize = 16.sp
+
+                                // Klik 2x
+                                ClickableText(
+                                    text = AnnotatedString("Klik 2x: ${totalClicksByCount.value[2] ?: 0}"),
+                                    style = TextStyle(
+                                        fontSize = 16.sp,
+                                        color = Color(0xFF60D500) // Hijau cerah
+                                    ),
+                                    onClick = {
+                                        val categoryWords = sharedPreferences.all
+                                            .filter { it.key.endsWith("_clickCount") && it.value == 2 }
+                                            .keys.map { it.removeSuffix("_clickCount") }
+                                        showCategoryWords.value = 2 to categoryWords
+                                    }
                                 )
-                                Text(
-                                    text = "Klik 3x: ${totalClicksByCount.value[3] ?: 0}",
-                                    color = Color(0xFF447446), // Tetap hijau tua
-                                    fontSize = 16.sp
+
+                                // Klik 3x
+                                ClickableText(
+                                    text = AnnotatedString("Klik 3x: ${totalClicksByCount.value[3] ?: 0}"),
+                                    style = TextStyle(
+                                        fontSize = 16.sp,
+                                        color = Color(0xFF447446) // Hijau tua
+                                    ),
+                                    onClick = {
+                                        val categoryWords = sharedPreferences.all
+                                            .filter { it.key.endsWith("_clickCount") && it.value == 3 }
+                                            .keys.map { it.removeSuffix("_clickCount") }
+                                        showCategoryWords.value = 3 to categoryWords
+                                    }
                                 )
+
+                                // Dialog untuk menampilkan kata-kata dalam kategori
+                                showCategoryWords.value?.let { (category, words) ->
+                                    var searchText by remember { mutableStateOf("") } // State untuk teks pencarian
+                                    val filteredWords = words.filter { it.contains(searchText, ignoreCase = true) } // Filter kata berdasarkan pencarian
+
+                                    AlertDialog(
+                                        onDismissRequest = { showCategoryWords.value = null },
+                                        title = {
+                                            Text("Kata-kata Klik ${category}x")
+                                        },
+                                        text = {
+                                            Column(
+                                                modifier = Modifier
+                                                    .fillMaxWidth()
+                                                    .padding(horizontal = 16.dp)
+                                            ) {
+                                                // Kolom Pencarian
+                                                TextField(
+                                                    value = searchText,
+                                                    onValueChange = { searchText = it },
+                                                    placeholder = { Text("Cari kata...") },
+                                                    modifier = Modifier
+                                                        .fillMaxWidth()
+                                                        .padding(bottom = 16.dp)
+                                                )
+
+                                                // Daftar Kata yang Dapat Diklik
+                                                LazyColumn(
+                                                    modifier = Modifier
+                                                        .fillMaxWidth()
+                                                        .padding(vertical = 16.dp)
+                                                ) {
+                                                    items(filteredWords) { word ->
+                                                        Box(
+                                                            modifier = Modifier
+                                                                .fillMaxWidth()
+                                                                .padding(8.dp)
+                                                                .clickable {
+                                                                    sendTextToExternalApp(context, word)
+                                                                }
+                                                        ) {
+                                                            Text(
+                                                                text = word,
+                                                                fontSize = 14.sp,
+                                                                modifier = Modifier.padding(8.dp)
+                                                            )
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        confirmButton = {
+                                            Button(onClick = { showCategoryWords.value = null }) {
+                                                Text("Tutup")
+                                            }
+                                        }
+                                    )
+                                }
                             }
                         },
                         colors = TopAppBarDefaults.smallTopAppBarColors(
